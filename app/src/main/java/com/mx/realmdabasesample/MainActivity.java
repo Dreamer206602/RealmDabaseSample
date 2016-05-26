34d55272e6dce00f8512e80c9381ed65b55bef65
Deamer206602 <boobooMX@163.com>
2016-05-26 17:02:31 +0800

    添加RxJava类库，并添加该Library所需要的依赖库

--- app/src/main/java/com/mx/realmdabasesample/MainActivity.java
+++ app/src/main/java/com/mx/realmdabasesample/MainActivity.java
@@ -1,13 +1,21 @@
 package com.mx.realmdabasesample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.rxJava)
+    Button mRxJava;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
